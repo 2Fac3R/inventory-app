6099cc4fb55429516292f7dae354b4f18faa293a
Gustavo Navarro <genr1818@gmail.com>
2022-08-10 13:51:27 -0500

    Recurso de Productos

---
+++ database/migrations/2022_08_10_183248_create_products_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->unsignedBigInteger('category_id');
+
+            $table->string('sku')->unique();
+            $table->string('name');
+            $table->text('description');
+            $table->float('price', 8, 2);
+            $table->integer('quantity');
+            $table->boolean('state')->default(0);
+
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
