7b905ed99ea38b9f70e6c75865fe1372adb61cef
Gustavo Navarro <genr1818@gmail.com>
2022-08-11 14:15:29 -0500

    ProductController routes

--- routes/web.php
+++ routes/web.php
@@ -15,17 +15,15 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
-
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
 
-require __DIR__.'/auth.php';
+Route::resource(
+    'products',
+    App\Http\Controllers\ProductController::class
+)->middleware('auth:sanctum');
+
+require __DIR__ . '/auth.php';
