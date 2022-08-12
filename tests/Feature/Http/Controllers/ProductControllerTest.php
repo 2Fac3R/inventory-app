9c566b8c88c39832cb5dae728acd8174372955e6
Gustavo Navarro <genr1818@gmail.com>
2022-08-12 16:16:27 -0500

    Validation tests

--- tests/Feature/Http/Controllers/ProductControllerTest.php
+++ tests/Feature/Http/Controllers/ProductControllerTest.php
@@ -77,6 +77,70 @@ class ProductControllerTest extends TestCase
         $this->assertDatabaseHas('products', $data);
     }
 
+    public function test_validation_sku_size()
+    {
+        $user = User::factory()->create();
+        $category = Category::factory()->create();
+        $data = [
+            'category_id' => $category->id,
+            'sku' => 'wrong-sku', // 8 size validation. 'wrong-sku' (9 characters)
+            'name' => $this->faker->word(),
+            'description' => $this->faker->text(800),
+            'price' => $this->faker->randomFloat(2, 0, 10000),
+            'quantity' => $this->faker->numberBetween(1, 100),
+            'state' => $this->faker->boolean($chanceOfGettingTrue = 50),
+        ];
+
+        $this->actingAs($user)
+            ->post('products', $data)
+            ->assertSessionHasErrors();
+
+        $this->assertDatabaseMissing('products', $data);
+    }
+
+    public function test_validation_sku_unique()
+    {
+        $user = User::factory()->create();
+        $category = Category::factory()->create();
+        Product::factory()->create(['sku' => 'aaaaaaaa']); // product_id = 1
+        $data = [
+            'category_id' => $category->id,
+            'sku' => 'aaaaaaaa', // already exists
+            'name' => $this->faker->word(),
+            'description' => $this->faker->text(800),
+            'price' => $this->faker->randomFloat(2, 0, 10000),
+            'quantity' => $this->faker->numberBetween(1, 100),
+            'state' => $this->faker->boolean($chanceOfGettingTrue = 50),
+        ];
+
+        $this->actingAs($user)
+            ->post('products', $data) // product_id = 2
+            ->assertSessionHasErrors();
+
+        $this->assertDatabaseMissing('products', $data);
+    }
+
+    public function test_validation_state_is_boolean()
+    {
+        $user = User::factory()->create();
+        $category = Category::factory()->create();
+        $data = [
+            'category_id' => $category->id,
+            'sku' => $this->faker->ean8(), // Generate a random EAN-8 barcode.
+            'name' => $this->faker->word(),
+            'description' => $this->faker->text(800),
+            'price' => $this->faker->randomFloat(2, 0, 10000),
+            'quantity' => $this->faker->numberBetween(1, 100),
+            'state' => 'not-boolean', // Not boolean type
+        ];
+
+        $this->actingAs($user)
+            ->post('products', $data)
+            ->assertSessionHasErrors();
+
+        $this->assertDatabaseMissing('products', $data);
+    }
+
     public function test_show()
     {
         $user = User::factory()->create();
